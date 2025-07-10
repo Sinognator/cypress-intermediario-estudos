6e2723c5fef48d59317118da309b9e58103ccf66
paulo.rocha <pr.moreirarocha>
2025-07-10 17:38:23 -0300

    Alterado checkout

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   video: true, // ✅ grava vídeos
-  videoUploadOnPasses: true, // ✅ mantém os vídeos mesmo se o teste passar
+  videoUploadOnPasses: false, // ✅ mantém os vídeos mesmo se o teste passar
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here, se precisar
