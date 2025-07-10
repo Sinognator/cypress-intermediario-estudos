da476861df3e56bb9fda4a203200b9f50a12a4ee
paulo.rocha <pr.moreirarocha>
2025-07-10 17:22:15 -0300

    Alterado cypressconfig

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,12 @@
-module.exports = {
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  video: true, // ✅ grava vídeos
+  videoUploadOnPasses: false, // ✅ mantém os vídeos mesmo se o teste passar
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      specPattern: 'cypress/e2e/**/*.cy.js' 
+      // implement node event listeners here, se precisar
     },
-  },
-};
+    specPattern: 'cypress/e2e/**/*.cy.js'
+  }
+});
